edb1f2af6f8e8ba0b83cc6ca0569bd98d6e3fc46
chess-loli <yrck666@gmail.com>
2019-09-03 16:02:36 +0200

    created a core for mastermind start

---
+++ mastermind.js
@@ -0,0 +1,23 @@
+Vue.component('mastermind', {
+    props: {
+
+    },
+    template: `
+    
+    `,
+    computed: {
+        
+    },
+})
+
+
+
+new Vue({
+    el: '#root',
+    data: {
+
+    },
+    methods: {
+        
+    },
+})
